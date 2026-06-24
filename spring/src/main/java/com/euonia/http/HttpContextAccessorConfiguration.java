fe59d743681b8abbe40b11691681665029d13064
damon <zhaorong@outlook.com>
2026-06-24 11:50:07 +0800

    refactor: simplify RequestContextAccessor bean configuration

--- spring/src/main/java/com/euonia/http/HttpContextAccessorConfiguration.java
+++ spring/src/main/java/com/euonia/http/HttpContextAccessorConfiguration.java
@@ -1,9 +1,7 @@
 package com.euonia.http;
 
-import com.euonia.core.ObjectId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.RequestScope;
 
 /**
  * Configuration for request context related beans.
@@ -12,13 +10,7 @@ import org.springframework.web.context.annotation.RequestScope;
 public class HttpContextAccessorConfiguration {
 
     @Bean
-    @RequestScope
-    public RequestContextAccessor requestContextAccessor(RequestContext context) {
-        if (context.getTraceIdentifier() == null || context.getTraceIdentifier().isBlank()) {
-            context.setTraceIdentifier(ObjectId.newGuid().toString());
-        }
-        var accessor = new DefaultRequestContextAccessor();
-        accessor.setRequestContext(context);
-        return accessor;
+    public RequestContextAccessor requestContextAccessor() {
+        return new DefaultRequestContextAccessor();
     }
 }
