68adad6c8c1703f07fcb25de2c51eeea8fc39be7
damon <zhaorong@outlook.com>
2026-06-25 14:13:46 +0800

    refactor: replace DefaultRequestContextAccessor with RequestContextAccessor in HttpContextFilter

--- sample/src/main/java/com/euonia/sample/HttpContextFilter.java
+++ sample/src/main/java/com/euonia/sample/HttpContextFilter.java
@@ -1,8 +1,8 @@
 package com.euonia.sample;
 
 import com.euonia.core.ObjectId;
-import com.euonia.http.DefaultRequestContextAccessor;
 import com.euonia.http.RequestContext;
+import com.euonia.http.RequestContextAccessor;
 import jakarta.servlet.*;
 import org.apache.catalina.connector.RequestFacade;
 import org.springframework.stereotype.Component;
@@ -20,10 +20,10 @@ public class HttpContextFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
             var context = getRequestContext((RequestFacade) request);
-            DefaultRequestContextAccessor.set(context);
+            RequestContextAccessor.set(context);
             chain.doFilter(request, response);
         } finally {
-            DefaultRequestContextAccessor.remove();
+            RequestContextAccessor.remove();
         }
     }
 
