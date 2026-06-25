4d28fab2f0237de347e2de514e52d3df86fea172
damon <zhaorong@outlook.com>
2026-06-25 14:04:39 +0800

    feat: implement KafkaTransport class to handle asynchronous message publishing and sending

--- bus-kafka/src/main/java/com/euonia/bus/KafkaTransport.java
+++ bus-kafka/src/main/java/com/euonia/bus/KafkaTransport.java
@@ -1,4 +1,32 @@
 package com.euonia.bus;
 
-public class KafkaTransport {
+import com.euonia.bus.contract.Transport;
+
+import java.util.concurrent.CompletableFuture;
+
+public final class KafkaTransport implements Transport {
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public <M> CompletableFuture<Void> publishAsync(RoutedMessage<M> message) {
+        return null;
+    }
+
+    @Override
+    public <M> CompletableFuture<Void> sendAsync(RoutedMessage<M> message) {
+        return null;
+    }
+
+    @Override
+    public <M, R> CompletableFuture<R> sendAsync(RoutedMessage<M> message, Class<R> responseType) {
+        return null;
+    }
+
+    @Override
+    public <M, R> CompletableFuture<R> callAsync(RoutedMessage<M> message, Class<R> responseType) {
+        return null;
+    }
 }
